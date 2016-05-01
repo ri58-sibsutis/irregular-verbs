a180b750f440f2b0037a96f9267423e2d2198a23
Ivan Potapov <vantapkin.1997@gmail.com>
2016-05-01 22:05:53 +0600

    Output response

--- counting_correct_answers.c
+++ counting_correct_answers.c
@@ -7,7 +7,12 @@ int counting_correct_answers(char verbs[], char userverbs[], int calc)//V OTDELN
     int i, flg = 1, n = 0;
     for (i = 0; verbs[i] != '\0'; i++) n = i;
     for(i = 0; i < n; i++)
-    {
 		if(verbs[i] != userverbs[i]) flg = 0;
-    }
+    if (flg == 1) 
+	{
+		calc++;
+		printf ("Правильно!\n");
+	}
+	else printf ("Не верно\n");
+	return calc;
 }
